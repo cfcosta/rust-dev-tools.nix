7db4da6aac0eff9459051379cee045d2d5744c61
Cainã Costa <me@cfcosta.com>
2023-09-14 11:08:17 -0300

    chore(rust): add audit, fmt and udeps script

--- modules/rust.nix
+++ modules/rust.nix
@@ -4,6 +4,7 @@ let
     pkgs.rust-bin.${channel}.${version}.default.override {
       extensions = [ "rust-src" "clippy" "rustfmt" "rust-analyzer" ];
     };
+  rustNightly = rust "nightly" "latest";
 
   watch = cmd: ''
     ${pkgs.cargo-watch}/bin/cargo-watch watch -s "${cmd}"
@@ -15,10 +16,19 @@ let
       (watch "${packageName}-${name}"))
   ];
 
-  check = "cargo clippy --tests --benches $@";
-  test = "${pkgs.cargo-nextest}/bin/cargo-nextest nextest run --release";
-  bench = "cargo bench $@";
-  doc = "cargo doc";
+  audit = "exec ${pkgs.cargo-audit}/bin/cargo-audit audit $@";
+  bench = "exec cargo bench $@";
+  check = "exec cargo clippy --tests --benches $@";
+  doc = "exec cargo doc $@";
+  fmt = "exec cargo fmt $@";
+  test = "exec ${pkgs.cargo-nextest}/bin/cargo-nextest nextest run";
+
+  # Nightly-only tools
+  udeps = ''
+    export RUSTC="${rustNightly}/bin/rustc";
+    export CARGO="${rustNightly}/bin/cargo";
+    exec "${pkgs.cargo-udeps}/bin/cargo-udeps" udeps $@
+  '';
 in {
   package = {
     latest = rust "stable" "latest";
@@ -26,9 +36,12 @@ in {
   };
 
   scripts = [
-    (script "check" check)
-    (script "test" test)
+    (script "audit" audit)
     (script "bench" bench)
+    (script "check" check)
     (script "doc" doc)
+    (script "fmt" fmt)
+    (script "test" test)
+    (script "udeps" udeps)
   ];
 }
