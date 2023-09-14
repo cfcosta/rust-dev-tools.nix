7e0e03d49d2c327211fe626b13255d403ff26fcd
Cainã Costa <me@cfcosta.com>
2023-09-14 11:27:21 -0300

    chore: expose utils inside instance

--- modules/default.nix
+++ modules/default.nix
@@ -1,4 +1,6 @@
 { packageName, pkgs, utils, inputs }: {
+  inherit utils;
+
   overlays.default = [ inputs.rust-overlay.overlays.default ];
 
   database = import ./database.nix { inherit packageName pkgs utils; };
