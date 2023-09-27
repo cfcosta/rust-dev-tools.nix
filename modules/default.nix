467ce7f3670c647cc85b124cae364ffbaf1dde72
Cainã Costa <me@cfcosta.com>
2023-09-27 11:00:30 -0300

    chore: remove nix module
    Let's only focus on rust right now.

--- modules/default.nix
+++ modules/default.nix
@@ -1,17 +1,13 @@
 { pkgs, utils, options }:
 let
-  modules = {
-    rust = import ./rust.nix { inherit options pkgs utils; };
-    nix = import ./nix.nix { inherit options pkgs; };
-  };
+  modules = { rust = import ./rust.nix { inherit options pkgs utils; }; };
 
   env = utils.deepMerge modules.rust.env options.env;
 in {
   inherit utils;
 
   devShell = pkgs.mkShell ({
-    packages =
-      [ (modules.nix.scripts) (modules.rust.findRust) (modules.rust.packages) ]
+    packages = [ (modules.rust.findRust) (modules.rust.packages) ]
       ++ options.dependencies;
     shellHook = if options.shellHook == null then "" else options.shellHook;
   } // env);
