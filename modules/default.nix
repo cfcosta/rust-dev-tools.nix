69150787f318ddd35ede900864d1d379dd7a3a3e
Cainã Costa <me@cfcosta.com>
2023-10-14 11:59:39 -0300

    feat: add proxy script to call subcommands

--- modules/default.nix
+++ modules/default.nix
@@ -2,13 +2,43 @@
 let
   modules.rust = import ./rust.nix { inherit options pkgs utils; };
   env = utils.deepMerge modules.rust.env options.env;
+
+  proxyCompletion = ''
+    _${options.name}_complete() {
+        local cur prev opts
+        COMPREPLY=()
+        cur="$\{COMP_WORDS[COMP_CWORD]\}"
+        prev="$\{COMP_WORDS[COMP_CWORD-1]\}"
+        opts="$(compgen -c | grep ^${options.name}- | sed 's/${options.name}-//')"
+
+        if [[ $cur == * ]]; then
+            COMPREPLY=( $(compgen -W "$opts" -- $cur) )
+            return 0
+        fi
+    }
+
+    complete -F _${options.name}_complete ${options.name}
+  '';
+
+  mainScript = pkgs.stdenv.mkDerivation {
+    name = "${options.name}-1.0.0";
+    version = "1.0.0";
+
+    src = ./scripts;
+    nativeBuildInputs = [ pkgs.installShellFiles ];
+
+    installPhase = ''
+      install -D $src/proxy.sh $out/bin/${options.name}
+      installShellCompletion --name ${options.name} --bash <(echo "${proxyCompletion}")
+    '';
+  };
 in {
   inherit utils;
 
   devShell = pkgs.mkShell ({
     inherit (options) shellHook;
 
-    packages = [ (modules.rust.findRust) (modules.rust.packages) ]
+    packages = [ mainScript modules.rust.findRust modules.rust.packages ]
       ++ options.dependencies;
   } // env);
 }
---
+++ modules/scripts/proxy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+execute() {
+	local cmd="$(basename "${0}")-${1}"
+
+	if command -v "${cmd}" &>/dev/null; then
+		"${cmd}" "${@:2}"
+	else
+		echo "Error: Command '$cmd' does not exist."
+		exit 1
+	fi
+}
+
+main() {
+	if [ "${#}" -lt 1 ]; then
+		echo "Usage: $(basename "${0}") <sub-command>"
+		exit 1
+	fi
+
+	local subcommand="$1"
+
+	subcommand="${subcommand/-/}" # Replace any hyphens with nothing
+	execute "${subcommand}" "${@:2}"
+}
+
+main "$@"
